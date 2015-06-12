aac862fe0904260dc0e8e595560a9fae2344df1d
gib-upm <gpadron@infomed.dia.fi.upm.es>
2015-06-12 14:00:30 +0200

    changed create user

--- bin/sql/create_user.sql
+++ bin/sql/create_user.sql
@@ -2,5 +2,6 @@ CREATE USER 'testCV'@'localhost' IDENTIFIED BY 'testCV';
 GRANT ALL ON `%testcv%`.* TO 'testCV'@'localhost';
 GRANT ALL ON normalization_errors.* TO 'testCV'@'localhost';
 GRANT ALL ON translations.* TO 'testCV'@'localhost';
+GRANT ALL ON metathesaurus.* TO 'testCV'@'localhost';
 GRANT CREATE ON *.* TO 'testCV'@'localhost';
 FLUSH PRIVILEGES;
